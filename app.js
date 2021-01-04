824c9c87ce0512db328a107699ffbf22c9a45613
Ashwani kumar <ashwan201kumar@gmail.com>
2021-01-04 15:12:10 +0530

    ee

--- app.js
+++ app.js
@@ -3,37 +3,32 @@ const bodyParser = require("body-parser");
 
 const app = express();
 
+var items = ["ashu", "modi", "lallu"];
+
 app.set('view engine', 'ejs');
 
-app.get('/', function(req, res){
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.get("/", function(req, res) {
 
     var today = new Date();
-    var currentDay = today.getDay();
-    var day = "";
-    switch(currentDay) {
-        case 0:
-        day = "Sunday";
-        case 1:
-        day = "Monday";
-        case 2:
-        day = "Tuesday";
-        case 3:
-        day = "Wednesday";
-        case 4:
-        day = "Thursday";
-        case 5:
-        day = "Friday";
-        case 6:
-        day = "Saturday";
-        break;
-        default:
-        console.log("Error: current day is equal to: " + currentDay);
-    }
-        res.render("list", {
-            KindofDay: day
-        });
     
-});
+    var options = {
+        weekday: "long",
+        day: "numeric",
+        month: "long"
+    };
+    var day = today.toLocaleDateString("en-US", options);
+    
+        res.render("list", {KindofDay: day, Itemlists: items});
+    });
+
+        app.post("/", function (req, res) {
+        var item = req.body.newItem;
+        items.push(item);
+        
+        res.redirect("/");
+        });
 
 app.listen(3000, function(){
     console.log("server is running on port no 3000");
